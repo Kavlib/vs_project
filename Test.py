e6fdaddb509f45fc5ff9aa5879047266058f9231
Libor Kavka <kavka@utb.cz>
2021-12-07 06:54:49 +0100

    Přidán soubo Test.py

---
+++ Test.py
@@ -0,0 +1,12 @@
+def product(a, b):
+    return a * b
+
+"""def test_product():
+    assert product(2, 4) == 8"""
+
+if __name__ == '__main__':
+    a = int(input('zadej hodnotu a:'))
+    b = int(input('zadej hodnotu b:'))
+    c = product(a, b)
+    print(c)
+    
